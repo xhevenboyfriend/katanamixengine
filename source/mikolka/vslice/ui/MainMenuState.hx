7d06d1466b3b2172963a8def48ad420dbeb9edf3
KatanaPeter <158230664+XHevenBoyfriend@users.noreply.github.com>
2026-01-13 21:20:36 -0300

    Update MainMenuState.hx

--- source/mikolka/vslice/ui/MainMenuState.hx
+++ source/mikolka/vslice/ui/MainMenuState.hx
@@ -5,135 +5,143 @@ import flixel.FlxSprite;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
+import mikolka.vslice.ui.mainmenu.DesktopMenuState;
+import mikolka.compatibility.ui.MainMenuHooks;
 import mikolka.compatibility.VsliceOptions;
+import mikolka.vslice.ui.title.TitleState;
 import mikolka.compatibility.ModsHelper;
+import options.OptionsState;
+import backend.Paths;
+import backend.MusicBeatState;
+import backend.DiscordClient;
 
 class MainMenuState extends MusicBeatState
 {
-    // Versões mantidas do código original [cite: 4, 10]
-    public static var pSliceVersion:String = '3.4';
-    public static var funkinVersion:String = '0.7.6';
+	public static var psychEngineVersion:String = '0.6.3'; 
+	public static var pSliceVersion:String = '3.4'; [cite: 4]
+	public static var funkinVersion:String = '0.7.6'; [cite: 4]
 
-    var bg:FlxSprite;
-    var sideBar:FlxSprite;
-    var character:FlxSprite;
-    var header:FlxSprite;
-    
-    var menuItems:FlxTypedGroup<FlxSprite>;
-    var optionShit:Array<String> = ['PLAY', 'FREEPLAY', 'OPTIONS', 'SHOP'];
-    var curSelected:Int = 0;
+	var bg:FlxSprite;
+	var sideBar:FlxSprite;
+	var character:FlxSprite;
+	var header:FlxSprite;
+	var magenta:FlxSprite; [cite: 5]
 
-    override function create()
-    {
-        // Limpeza de cache baseada no MainMenuState original [cite: 6]
-        CacheSystem.clearStoredMemory();
-        CacheSystem.clearUnusedMemory();
+	var menuItems:FlxTypedGroup<FlxSprite>;
+	var optionShit:Array<String> = ['PLAY', 'FREEPLAY', 'OPTIONS', 'SHOP'];
+	var curSelected:Int = 0;
+	var stickerSubState:Bool; [cite: 5]
 
-        persistentUpdate = persistentDraw = true;
+	public function new(?stickers:Bool = false)
+	{
+		super();
+		stickerSubState = stickers; [cite: 5]
+	}
 
-        // 1. Fundo Quadriculado (bg.png)
-        bg = new FlxSprite().loadGraphic(Paths.image('lobbymainmenu/bg'));
-        bg.antialiasing = VsliceOptions.ANTIALIASING; [cite: 8]
-        bg.scrollFactor.set(0, 0);
-        bg.screenCenter();
-        add(bg);
+	override function create()
+	{
+		#if DISCORD_ALLOWED
+		DiscordClient.changePresence("In the Menus", null); [cite: 7]
+		#end
 
-        // 2. Linha/Barra Lateral (line_purple.png)
-        sideBar = new FlxSprite(500, 0).loadGraphic(Paths.image('lobbymainmenu/line_purple'));
-        sideBar.antialiasing = VsliceOptions.ANTIALIASING;
-        sideBar.scrollFactor.set(0, 0);
-        add(sideBar);
+		if(stickerSubState) ModsHelper.clearStoredWithoutStickers(); [cite: 6]
+		else CacheSystem.clearStoredMemory(); [cite: 6]
+		CacheSystem.clearUnusedMemory(); [cite: 6]
 
-        // 3. Personagem (nexus.png)
-        character = new FlxSprite(650, 150).loadGraphic(Paths.image('lobbymainmenu/nexus'));
-        character.antialiasing = VsliceOptions.ANTIALIASING;
-        character.scrollFactor.set(0, 0);
-        character.setGraphicSize(Std.int(character.width * 0.8));
-        add(character);
+		persistentUpdate = persistentDraw = true;
 
-        // 4. Cabeçalho (menu display.png)
-        header = new FlxSprite(40, 40).loadGraphic(Paths.image('lobbymainmenu/menu display'));
-        header.antialiasing = VsliceOptions.ANTIALIASING;
-        header.scrollFactor.set(0, 0);
-        add(header);
+		// 1. Fundo (assets/shared/images/lobbymainmenu/bg.png)
+		bg = new FlxSprite().loadGraphic(Paths.image('lobbymainmenu/bg')); [cite: 7]
+		bg.antialiasing = VsliceOptions.ANTIALIASING; [cite: 8]
+		bg.screenCenter();
+		add(bg);
 
-        // 5. Botões do Menu (PLAY, FREEPLAY, OPTIONS, SHOP)
-        menuItems = new FlxTypedGroup<FlxSprite>();
-        add(menuItems);
+		// 2. Barra Lateral (assets/shared/images/lobbymainmenu/line_purple.png)
+		sideBar = new FlxSprite(550, 0).loadGraphic(Paths.image('lobbymainmenu/line_purple'));
+		sideBar.antialiasing = VsliceOptions.ANTIALIASING;
+		add(sideBar);
 
-        for (i in 0...optionShit.length)
-        {
-            var menuItem:FlxSprite = new FlxSprite(0, 0);
-            // Busca cada botão na pasta lobbymainmenu
-            menuItem.loadGraphic(Paths.image('lobbymainmenu/' + optionShit[i]));
-            menuItem.antialiasing = VsliceOptions.ANTIALIASING;
-            menuItem.ID = i;
+		// 3. Personagem (assets/shared/images/lobbymainmenu/nexus.png)
+		character = new FlxSprite(600, 100).loadGraphic(Paths.image('lobbymainmenu/nexus'));
+		character.antialiasing = VsliceOptions.ANTIALIASING;
+		character.setGraphicSize(Std.int(character.width * 0.8));
+		character.updateHitbox();
+		add(character);
 
-            // Organização em Grid conforme a screenshot LobbyMenu.png
-            var col:Int = i % 2;
-            var row:Int = Std.int(i / 2);
+		// 4. Cabeçalho (assets/shared/images/lobbymainmenu/menu display.png)
+		header = new FlxSprite(40, 40).loadGraphic(Paths.image('lobbymainmenu/menu display'));
+		header.antialiasing = VsliceOptions.ANTIALIASING;
+		add(header);
 
-            menuItem.x = 60 + (col * 240);
-            menuItem.y = 250 + (row * 180);
-            
-            menuItems.add(menuItem);
-        }
+		// 5. Botões
+		menuItems = new FlxTypedGroup<FlxSprite>();
+		add(menuItems);
 
-        // Rodapé com versões [cite: 10, 11]
-        var fnfVer:FlxText = new FlxText(12, FlxG.height - 24, 0, 'v${funkinVersion} (P-slice ${pSliceVersion})', 12);
-        fnfVer.scrollFactor.set();
-        fnfVer.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, OUTLINE, FlxColor.BLACK);
-        add(fnfVer);
+		for (i in 0...optionShit.length)
+		{
+			var menuItem:FlxSprite = new FlxSprite(0, 0);
+			menuItem.loadGraphic(Paths.image('lobbymainmenu/' + optionShit[i]));
+			menuItem.antialiasing = VsliceOptions.ANTIALIASING;
+			menuItem.ID = i;
 
-        changeSelection();
+			var col:Int = i % 2;
+			var row:Int = Std.int(i / 2);
 
-        super.create();
-    }
+			menuItem.x = 80 + (col * 260);
+			menuItem.y = 280 + (row * 160);
+			menuItems.add(menuItem);
+		}
 
-    override function update(elapsed:Float)
-    {
-        if (FlxG.sound.music.volume < 0.8)
-            FlxG.sound.music.volume += 0.5 * elapsed;
+		// Versões [cite: 10, 11]
+		var fnfVer:FlxText = new FlxText(12, FlxG.height - 24, 0, 'v${funkinVersion} (P-slice ${pSliceVersion})', 12);
+		fnfVer.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, OUTLINE, FlxColor.BLACK);
+		add(fnfVer);
 
-        if (controls.UI_LEFT_P) changeSelection(-1);
-        if (controls.UI_RIGHT_P) changeSelection(1);
-        if (controls.UI_UP_P) changeSelection(-2);
-        if (controls.UI_DOWN_P) changeSelection(2);
+		changeSelection();
+		super.create();
+	}
 
-        if (controls.BACK)
-        {
-            FlxG.sound.play(Paths.sound('cancelMenu'));
-            MusicBeatState.switchState(new TitleState()); [cite: 1]
-        }
+	override function update(elapsed:Float)
+	{
+		if (FlxG.sound.music.volume < 0.8)
+			FlxG.sound.music.volume += 0.5 * elapsed; [cite: 15]
 
-        if (controls.ACCEPT)
-        {
-            var daChoice:String = optionShit[curSelected];
-            switch (daChoice)
-            {
-                case 'FREEPLAY':
-                    MusicBeatState.switchState(new mikolka.vslice.freeplay.FreeplayState());
-                case 'OPTIONS':
-                    MusicBeatState.switchState(new options.OptionsState());
-            }
-        }
+		if (controls.UI_LEFT_P) changeSelection(-1);
+		if (controls.UI_RIGHT_P) changeSelection(1);
+		if (controls.UI_UP_P) changeSelection(-2);
+		if (controls.UI_DOWN_P) changeSelection(2);
 
-        super.update(elapsed);
-    }
+		if (controls.BACK)
+		{
+			FlxG.sound.play(Paths.sound('cancelMenu'));
+			MusicBeatState.switchState(new TitleState());
+		}
 
-    function changeSelection(change:Int = 0)
-    {
-        curSelected += change;
-        if (curSelected < 0) curSelected = optionShit.length - 1;
-        if (curSelected >= optionShit.length) curSelected = 0;
+		if (controls.ACCEPT)
+		{
+			var daChoice:String = optionShit[curSelected];
+			switch (daChoice)
+			{
+				case 'FREEPLAY':
+					MusicBeatState.switchState(new mikolka.vslice.freeplay.FreeplayState());
+				case 'OPTIONS':
+					MusicBeatState.switchState(new OptionsState()); [cite: 13]
+			}
+		}
+		super.update(elapsed);
+	}
 
-        menuItems.forEach(function(spr:FlxSprite)
-        {
-            spr.alpha = 0.6;
-            if (spr.ID == curSelected) spr.alpha = 1;
-            spr.updateHitbox();
-        });
+	function changeSelection(change:Int = 0)
+	{
+		curSelected += change;
+		if (curSelected < 0) curSelected = optionShit.length - 1;
+		if (curSelected >= optionShit.length) curSelected = 0;
 
-        if (change != 0) FlxG.sound.play(Paths.sound('scrollMenu'));
-    }
+		menuItems.forEach(function(spr:FlxSprite)
+		{
+			spr.alpha = 0.6;
+			if (spr.ID == curSelected) spr.alpha = 1;
+		});
+		if (change != 0) FlxG.sound.play(Paths.sound('scrollMenu'));
+	}
 }
